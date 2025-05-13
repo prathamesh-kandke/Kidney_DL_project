9344375cc815575c6eb4a6d67be82de53bc6d885
prathamesh-kandke <prathameshkandke71@gmail.com>
2025-05-13 18:04:46 +0530

    docker file with cicd

--- src/cnnClassifier/pipeline/model_eval.py
+++ src/cnnClassifier/pipeline/model_eval.py
@@ -6,7 +6,7 @@ from src.cnnClassifier.config.configuration import ConfigurationManager
 from src.cnnClassifier.components.model_eval_04 import Evaluation
 from src.cnnClassifier import logger
 import os
-import mlflow
+#import mlflow
 
 
 STAGE_NAME = "Evaluation stage"
@@ -22,13 +22,13 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/prathamesh-kandke/Kidney_DL_project.mlflow"
-        os.environ["MLFLOW_TRACKING_USERNAME"]="prathamesh-kandke"
-        os.environ["MLFLOW_TRACKING_PASSWORD"]="4565d9a2170b1258c4b5afbb167b2eed6e593392"
+        # os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/prathamesh-kandke/Kidney_DL_project.mlflow"
+        # os.environ["MLFLOW_TRACKING_USERNAME"]="prathamesh-kandke"
+        # os.environ["MLFLOW_TRACKING_PASSWORD"]="4565d9a2170b1258c4b5afbb167b2eed6e593392"
 
         # # Set MLflow tracking URI
-        mlflow.set_tracking_uri("https://dagshub.com/prathamesh-kandke/Kidney_DL_project.mlflow")
-        evaluation.log_into_mlflow()
+        # mlflow.set_tracking_uri("https://dagshub.com/prathamesh-kandke/Kidney_DL_project.mlflow")
+        # evaluation.log_into_mlflow()
 
 
 
